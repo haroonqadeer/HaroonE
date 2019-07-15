94d1dc11ca6356bc957bda0b987a99544eb95481
AdnanAli7 <adnanali1990@gmail.com>
2019-07-15 08:32:38 +0500

    adnan login final

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -18,20 +18,10 @@ declare var $: any;
 })
 export class LoginComponent implements OnInit {
 
-// <<<<<<< HEAD
-    serverUrl = "http://125.1.1.252:9010/";
-// =======
-    //<<<<<<< HEAD
-    // serverUrl = "https://localhost:5001/";
 
-    //serverUrl = "https://localhost:8008/";
-    //serverUrl = "http://localhost:11664/";
-    //=======
-    //serverUrl = "http://192.168.200.52:1001/";
-// >>>>>>> 6baa61dccfa396920caf44af3017542ec596dd83
-    //serverUrl = "http://localhost:23145/";
-    //>>>>>>> 3989d7fefbd36ef29be1f3d121ba076c14d8cbf9
-    //serverUrl = "http://192.168.200.19:3006/";
+    //serverUrl = "http://125.1.1.252:9010/";
+    serverUrl = "http://localhost:23145";
+
     tokenKey = "token";
 
     httpOptions = {
