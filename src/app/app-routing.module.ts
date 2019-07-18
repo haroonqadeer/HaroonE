bc3811d91e92c5f35f2e73e8975080ba0b1568c8
nabeelfarman <knabeel380@gmail.com>
2019-07-18 15:38:45 +0500

    changes(18-7-2019)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,13 +18,13 @@ const routes: Routes = [
   },
   {
     path: 'Config',
-    loadChildren: () => ConfigSharedModule
-    // loadChildren: '../../projects/HaroonConfig/src/app/app.module#ConfigSharedModule'
+    // loadChildren: () => ConfigSharedModule
+    loadChildren: '../../projects/HaroonConfig/src/app/app.module#ConfigSharedModule'
   },
   {
     path: 'HR',
-    loadChildren: () => HRSharedModule
-    // loadChildren: '../../projects/HaroonH/src/app/app.module#HRSharedModule'
+    // loadChildren: () => HRSharedModule
+    loadChildren: '../../projects/HaroonH/src/app/app.module#HRSharedModule'
 
   }
 ];
