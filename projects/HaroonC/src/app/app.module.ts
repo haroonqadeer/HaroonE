64c3a50b2d8282ef6ae06c651ccd434ff80c939d
AdnanAli7 <adnanali1990@gmail.com>
2019-08-20 09:39:27 +0500

    changes by adnan (20-08-2019)

--- projects/HaroonC/src/app/app.module.ts
+++ projects/HaroonC/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BranchComponent } from './components/branch/branch.component';
+import { CompanyComponent } from './components/company/company.component';
+import { CompanydashboardComponent } from './components/companydashboard/companydashboard.component';
+import { CurrencyComponent } from './components/currency/currency.component';
+import { DepartmentComponent } from './components/department/department.component';
+import { HeadquarterComponent } from './components/headquarter/headquarter.component';
+import { SectionComponent } from './components/section/section.component';
+import { SubsidiarieComponent } from './components/subsidiarie/subsidiarie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BranchComponent,
+    CompanyComponent,
+    CompanydashboardComponent,
+    CurrencyComponent,
+    DepartmentComponent,
+    HeadquarterComponent,
+    SectionComponent,
+    SubsidiarieComponent
   ],
   imports: [
     BrowserModule,
--- projects/HaroonU/src/app/app.module.ts
+++ projects/HaroonU/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserprofileComponent } from './components/userprofile/userprofile.component';
+import { UserrolesComponent } from './components/userroles/userroles.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreatepasswordComponent } from './components/createpassword/createpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserprofileComponent,
+    UserrolesComponent,
+    DashboardComponent,
+    CreatepasswordComponent
   ],
   imports: [
     BrowserModule,
