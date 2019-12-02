d70dba528962d18ab8770949dab0ed68daf60ab9
nabeelfarman <knabeel380@gmail.com>
2019-12-02 11:50:50 +0500

    changes(2-12-2019)

--- projects/HaroonC/src/app/components/company/company.component.ts
+++ projects/HaroonC/src/app/components/company/company.component.ts
@@ -67,7 +67,7 @@ export class CompanyComponent implements OnInit {
     companyBox = true;
 
     //serverUrl = "http://ambit.southeastasia.cloudapp.azure.com:9043/";
-    serverUrl = "http://localhost:7007/";
+    serverUrl = "http://localhost:5000/";
     tokenKey = "token";
 
     httpOptions = {
