f5f9938b132a1ef0e4f3f8500d74ff4f7defd890
AdnanAli7 <adnanali1990@gmail.com>
2019-12-18 10:15:36 +0500

    adnan changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,7 @@ export class AppComponent implements OnInit{
 	}
 
   //modules variable declaration
-	moduleHR = true;
+	moduleHR = false;
 	moduleConfig = false;
 	moduleUM = true;
 	moduleComp = false;
