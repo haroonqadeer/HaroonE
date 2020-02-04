1b3da232363a173125e1e64faa82a587da8e539e
AdnanAli7 <adnanali1990@gmail.com>
2020-02-04 17:13:00 +0500

    CHANGES BY ADNAN

--- projects/HaroonH/src/app/components/attendance/attendance.component.ts
+++ projects/HaroonH/src/app/components/attendance/attendance.component.ts
@@ -19,105 +19,314 @@ export class AttendanceComponent implements OnInit {
 
   tokenKey = "token";
 
-  httpOptions = {
-    headers: new HttpHeaders({ "Content-Type": "application/json" })
-  };
+    httpOptions = {
+        headers: new HttpHeaders({ "Content-Type": "application/json" })
+    };
 
   //*Bolean variable
-  updateFlag = false;
-  disableFlag = true;
+    updateFlag = false;
+    disableFlag = false;
 
   //* list variables
-  brachList = [];
-  deptList = [];
-  sectnList = [];
-  employeeList = [];
-  empAttDetList = [];
-  managerList = [];
-  breakTypeList = [];
-  empBreakList = [];
+    brachList = [];
+    deptList = [];
+    sectnList = [];
+    employeeList = [];
+    tempEmployeeList = [];
+    empAttDetList = [];
+    managerList = [];
+    breakTypeList = [];
+    empBreakList = [];
+    attTypeList = [];
 
-  tempAttDetList = [];
-  tempSectnList = [];
-  excelDataList = [];
+    tempAttDetList = [];
+    tempSectnList = [];
+    excelDataList = [];
 
-  //* variables for pagination and orderby pipe
-  p = 1;
-  order = "info.name";
-  reverse = false;
-  sortedCollection: any[];
-  itemPerPage = "10";
+    //* variables for pagination and orderby pipe
+    p = 1;
+    order = "info.name";
+    reverse = false;
+    sortedCollection: any[];
+    itemPerPage = "10";
 
-  //*hidden variables
-  EmpId;
-  EmpCalCd;
-  attStatus;
-  editFlag = false;
+    //*hidden variables
+    EmpId;
+    EmpCalCd;
 
-  myTimeIn;
-  myTimeOut;
-  myDeptCd = 0;
-  myDtID = 0;
-  myShifhCd = 0;
+    editFlag = false;
 
-  //* Variables for NgModels
-  tblSearch;
+    //* Variables for NgModels
+    tblSearch;
 
-  attendanceDate;
-  ddlBranch;
-  ddlDepartment;
-  ddlSection;
-  ddlBreakType;
-  ddlManager;
+    attendanceDate;
+    ddlEmployee;
+    ddlAttType;
+    txtPassword;
 
-  attTime;
-  attRemarks;
+    myTimeIn;
+    myTimeOut;
+    myDeptCd = 0;
+    myDtID = 0;
+    myShifhCd = 0;  
 
-  lblBPS;
-  lblJobType;
-  lblEmployee;
+    txtdPassword = "";
+    txtdPin = "";
 
-  chkAddBreak = false;
 
-  txtdPassword = "";
-  txtdPin = "";
+    available = false;
+    absent = false;
+    inOffice = false;
+    onField = false;
+    onLeave = false;
+    remote = false;
 
-  constructor(
-    private _formBuilder: FormBuilder,
-    private toastr: ToastrManager,
-    private http: HttpClient,
-    private app: AppComponent
-  ) {}
+    constructor(
+        private _formBuilder: FormBuilder,
+        private toastr: ToastrManager,
+        private http: HttpClient,
+        private app: AppComponent
+    ) {}
 
-  ngOnInit() {
-    //this.attendanceDate = new Date();
+    ngOnInit() {
+        //this.brachList = this.app.branchList;
+        //this.ddlBranch = this.app.locationId;
 
-    //this.brachList = this.app.branchList;
-    //this.ddlBranch = this.app.locationId;
+        this.getEmployee();
+        this.getAttType();
+    }
 
-    this.getEmployee();
-  }
+    getEmployee() {
 
-  getEmployee() {
+        this.app.showSpinner();
+        //var Token = localStorage.getItem(this.tokenKey);
+        //var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + Token });
+        var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    this.app.showSpinner();
-    //var Token = localStorage.getItem(this.tokenKey);
-    //var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + Token });
-    var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json' });
+        this.http.get(this.serverUrl + 'api/getEmployee', { headers: reqHeader }).subscribe((data: any) => {
+            
+            //this.employeeListMain = data;
+            // for (var i = 0; i < data.length; i++) {
+            //     this.employeeList.push(data[i]);
+            // }
 
-    this.http.get(this.serverUrl + 'api/getEmployee', { headers: reqHeader }).subscribe((data: any) => {
-        
-        //this.employeeListMain = data;
-        for (var i = 0; i < data.length; i++) {
-            this.employeeList.push(data[i]);
+            this.employeeList = data;
+            this.tempEmployeeList = data;
+
+            this.app.hideSpinner();
+        });
+
+    }
+
+    getAttType() {
+
+        this.app.showSpinner();
+        //var Token = localStorage.getItem(this.tokenKey);
+        //var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + Token });
+        var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+        this.http.get(this.serverUrl + 'api/getAttType', { headers: reqHeader }).subscribe((data: any) => {
+            
+            this.attTypeList = data;
+
+            this.app.hideSpinner();
+        });
+
+    }
+
+    //Function for save attendance
+    save() {
+
+        if (this.ddlEmployee == undefined || this.ddlEmployee == "") {
+            this.toastr.errorToastr("Please select employee", "Error", {toastTimeout: 2500});
+            return false;
+        } else if (this.txtPassword == undefined || this.txtPassword == "") {
+            this.toastr.errorToastr("Please enter code", "Error", {toastTimeout: 2500});
+            return false;
+        } else if (this.disableFlag == false && (this.ddlAttType == undefined || this.ddlAttType == "")) {
+            this.toastr.errorToastr("Please select availibility", "Error", {toastTimeout: 2500});
+            return false;
+        }else {
+
+            var attDateTime = new Date();
+            var remarks = "-";
+
+            var myAddBreak = "No";
+            var attType;
+            var BreakTypeId;
+
+            if (this.myTimeIn == null && this.myTimeOut == null) {
+                this.myTimeIn = new Date();
+                this.myTimeOut = null;
+            } 
+            else if (this.myTimeIn != null && this.myTimeOut == null) {
+                this.myTimeOut = new Date();
+            } 
+            else if (this.myTimeIn != null && this.myTimeOut != null) {
+                myAddBreak = "Yes";
+                this.myTimeIn = new Date(); // this.myTimeOut;
+                //this.myTimeOut = 
+            }
+
+
+            if (this.disableFlag == true ){
+                attType = 1;
+            }else {
+                attType = this.ddlAttType;
+            }
+
+            //+ "DeptCd = " + this.myDeptCd + " --- " + "IndvdlID = " + this.EmpId + " ---- " + "DtID = " + this.myDtID + " --- " + "ShiftCd = " + this.myShifhCd + " --- " + "AttendanceStatCd = " + this.attStatus + " --- "
+            //alert("AddBreak = " +  myAddBreak + " --- " + "ApprvngManagerID = " + ManagerId + " --- " + "TimeIn = " + this.myTimeIn + " --- " + "TimeOut = " + this.myTimeOut + " --- " + "Rsn = " + this.attRemarks + " --- " + "TypeCd = " + BreakTypeId);
+            // return false;
+
+            //alert("AddBreak = " +  myAddBreak + " --- " + "TimeIn = " + this.myTimeIn + " --- " + "TimeOut = " + this.myTimeOut + " --- " + "Rsn = " + remarks);
+            //return false;
+
+            this.app.showSpinner();
+
+            //* ********************************************save data
+            var saveData = {
+                IndvdlID: this.ddlEmployee,
+                AttendanceStatCd: attType,
+                Code: this.txtPassword,
+                AddBreak: myAddBreak,
+                TimeIn: this.myTimeIn,
+                TimeOut: this.myTimeOut,
+                DeptCd: this.myDeptCd,
+                DtID: this.myDtID,
+                ShiftCd: this.myShifhCd,
+                ApprvngManagerID: 0,
+                Rsn: remarks,
+                TypeCd: 1, //BreakTypeId,
+                ConnectedUser: this.app.empId,
+                DelFlag: 0
+            };
+
+            //var token = localStorage.getItem(this.tokenKey);
+
+            //var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token });
+
+            var reqHeader = new HttpHeaders({ "Content-Type": "application/json" });
+
+            this.http.post(this.serverUrl + "api/saveEmpAttendance", saveData, { headers: reqHeader }) .subscribe((data: any) => {
+                if (data.msg != "Record Saved Successfully!") {
+                    this.app.hideSpinner();
+                    this.toastr.errorToastr(data.msg, "Error!", { toastTimeout: 5000 });
+                    return false;
+                } else {
+                    this.app.hideSpinner(); 
+                    this.toastr.successToastr(data.msg, "Success!", { toastTimeout: 2500 });
+                    this.getEmployee();
+                    this.clear();
+                    return false;
+                }
+            });
+        }
+    }
+
+    clear(){
+
+        this.ddlEmployee = "";
+        this.txtPassword = "";
+        this.ddlAttType = "";
+        this.myTimeIn = "";
+        this.myTimeOut = "";
+        this.disableFlag = false;
+        this.myDeptCd = 0;
+        this.myShifhCd = 0;
+        this.myDtID = 0;
+
+    }
+
+    //function for get filtere list
+    getFilterItem(filterOption) {
+        var dataList = [];
+
+        if (filterOption == "emp") {
+
+            dataList = this.employeeList.filter( x => x.empID == this.ddlEmployee );
+
+            if(dataList[0].timeIn == null){
+                //alert('null called');
+                this.myTimeIn = null;
+                this.myTimeOut = null;
+                this.myDtID = dataList[0].dtID;
+                this.myShifhCd = dataList[0].shiftCd;
+                this.myDeptCd = dataList[0].deptCd;
+
+            }
+            else{
+                //alert('not null called');
+                this.myTimeIn = dataList[0].timeIn;
+                this.myTimeOut = dataList[0].timeOut;
+                this.myDtID = dataList[0].dtID;
+                this.myShifhCd = dataList[0].shiftCd;
+                this.myDeptCd = dataList[0].deptCd;
+
+                if(dataList[0].timeOut == null){
+                    this.disableFlag = true;
+                }
+            }
+
+            if(dataList[0].managerJobPostDeptCd == 0){ 
+                this.myDeptCd = dataList[0].jobPostDeptCd;
+            }else{
+                this.myDeptCd = dataList[0].managerJobPostDeptCd;
+            }
+
         }
 
-        this.app.hideSpinner();
-    });
 
-}
+        if (filterOption == "fws") {
+
+            var available = null;
+            var absent = null;
+            var inOffice = null;
+            var onField = null;
+            var onLeave = null;
+            var remote = null; 
+
+            if(this.available == true){
+                available = 2;
+            }
+
+            if(this.absent == true){
+                absent = 3;
+            }
+
+            if(this.inOffice == true){
+                inOffice = 4;
+            }
+
+            if(this.onField == true){
+                onField = 5;
+            }
+
+            if(this.onLeave == true){
+                onLeave = 6;
+            }
+
+            if(this.remote == true){
+                remote = 7;
+            }
 
 
+            if (available == null && absent == null && inOffice == null && onField == null && onLeave == null && remote == null ){
+                this.tempEmployeeList = this.employeeList;
+            }else {
+                dataList = this.employeeList.filter( 
+                    x => x.attendanceStatCd == available
+                    ||  x.attendanceStatCd == absent
+                    || x.attendanceStatCd == inOffice
+                    || x.attendanceStatCd == onField
+                    || x.attendanceStatCd == onLeave
+                    || x.attendanceStatCd == remote
+                );
 
-  
+                this.tempEmployeeList = dataList;
+            }
+
+        }
+    }
+
 }
